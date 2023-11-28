945c0e4b7360089ddfec7985c90de99a01bd28e1
Nebot774 <adriclemenbot@gmail.com>
2023-11-28 17:21:41 +0100

    Finalizado co nerror raro:AAPT2 process unexpectedly exit. Error output:                           aapt2 W 11-28 17:21:04 16295 16295 LoadedArsc.cpp:657] Unknown chunk type '200'.

--- app/src/main/java/com/company/recyclerview/RecyclerElementosFragment.java
+++ app/src/main/java/com/company/recyclerview/RecyclerElementosFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RatingBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.company.recyclerview.databinding.FragmentRecyclerElementosBinding;
+import com.company.recyclerview.databinding.ViewholderElementoBinding;
+
+import java.util.List;
 
 
 public class RecyclerElementosFragment extends Fragment {
@@ -20,6 +27,77 @@ public class RecyclerElementosFragment extends Fragment {
     private FragmentRecyclerElementosBinding binding;
     private ElementosViewModel elementosViewModel;
     private NavController navController;
+
+    class ElementosAdapter extends RecyclerView.Adapter<ElementoViewHolder> {
+
+        // referencia al Array que obtenemos del ViewModel
+        List<Elemento> elementos;
+
+
+        ElementosViewModel elementosViewModel;
+
+        public ElementosAdapter(ElementosViewModel elementosViewModel) {
+            this.elementosViewModel = elementosViewModel; // Constructor modificado
+        }
+
+        // crear un nuevo ViewHolder
+        @NonNull
+        @Override
+        public ElementoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext()); // Cambio aquí
+            return new ElementoViewHolder(ViewholderElementoBinding.inflate(inflater, parent, false));
+        }
+
+        // rellenar un ViewHolder en una posición del Recycler con los datos del elemento que
+        // esté en esa misma posición en el Array
+        @Override
+        public void onBindViewHolder(@NonNull ElementoViewHolder holder, int position) {
+            Elemento elemento = elementos.get(position);
+
+            // Accediendo a binding a través del método getter
+            holder.getBinding().nombre.setText(elemento.nombre);
+            holder.getBinding().valoracion.setRating(elemento.valoracion);
+
+            holder.getBinding().valoracion.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                    if(fromUser) {
+                        elementosViewModel.actualizar(elemento, rating);
+                    }
+                }
+            });
+
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    elementosViewModel.seleccionar(elemento);
+                    navController.navigate(R.id.action_recyclerElementosFragment_to_mostrarElementoFragment);
+                }
+            });
+
+        }
+
+
+        // informar al Recycler de cuántos elementos habrá en la lista
+        @Override
+        public int getItemCount() {
+            return elementos != null ? elementos.size() : 0;
+        }
+
+        // establecer la referencia a la lista, y notificar al Recycler para que se regenere
+        public void establecerLista(List<Elemento> elementos){
+            this.elementos = elementos;
+            notifyDataSetChanged();
+        }
+
+
+
+        public Elemento obtenerElemento(int posicion){
+            return elementos.get(posicion);
+        }
+
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,9 +108,11 @@ public class RecyclerElementosFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+
         elementosViewModel = new ViewModelProvider(requireActivity()).get(ElementosViewModel.class);
         navController = Navigation.findNavController(view);
 
+        // navegar a NuevoElemento cuando se hace click en el FloatingActionButton
         binding.irANuevoElemento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +120,59 @@ public class RecyclerElementosFragment extends Fragment {
             }
         });
 
+        // crear el Adaptador
+        ElementosAdapter elementosAdapter = new ElementosAdapter(elementosViewModel);
+
+        // asociar el Adaptador con el RecyclerView
+        binding.recyclerView.setAdapter(elementosAdapter);
+
+        // obtener el array de Elementos, y pasarselo al Adaptador
+        elementosViewModel.obtener().observe(getViewLifecycleOwner(), new Observer<List<Elemento>>() {
+            @Override
+            public void onChanged(List<Elemento> elementos) {
+                elementosAdapter.establecerLista(elementos);
+            }
+        });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                ItemTouchHelper.RIGHT  | ItemTouchHelper.LEFT) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int posicion = viewHolder.getAdapterPosition();
+                Elemento elemento = elementosAdapter.obtenerElemento(posicion);
+                elementosViewModel.eliminar(elemento);
+
+            }
+        }).attachToRecyclerView(binding.recyclerView);
 
     }
-}
+    }
+
+
+
+    // Clase para inicializar el ViewBinding en los ViewHolder
+    class ElementoViewHolder extends RecyclerView.ViewHolder {
+        private final ViewholderElementoBinding binding;
+
+        public ElementoViewHolder(ViewholderElementoBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        // Método público para acceder al binding
+        public ViewholderElementoBinding getBinding() {
+            return binding;
+        }
+    }
+
+
+
+
+
